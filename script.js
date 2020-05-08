c31b3b7f59215f7f1283c2ee67cf266ffbe568bd
ItsDarix <darixmcpe@gmail.com>
2020-05-08 15:59:59 +0300

    3

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let x = 0;
 alert ('Это программа по вычислению квадратов различных чисел');
 for (i = 0; i < 1; i++) {
     let prompt1 = +(prompt('Введите число: '));
-    if (prompt1 != '' || null) {
-        x = prompt1*prompt1;
-        alert ('Квадрат числа ' + prompt1 + ' равен ' + x + '.');
+    if (prompt1 == '' || typeof (prompt1) == null || isNaN(prompt1)) {
+        alert ('Вы ввели не число!');
         i = i - 1;
     } else {
-        alert ('Вы ввели не число!');
+        x = prompt1*prompt1;
+        alert ('Квадрат числа ' + prompt1 + ' равен ' + x + '.');
         i = i - 1;
     }
 }
